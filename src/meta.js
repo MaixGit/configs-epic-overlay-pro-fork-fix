2f6d2aaf1e9593162433a2acc7ef070928d6edad
MaixGit <43832909+MaixGit@users.noreply.github.com>
2026-01-08 14:32:58 +1100

    Update meta.js

--- src/meta.js
+++ src/meta.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         configs epic overlay pro fork
+// @name         configs epic overlay pro fork fix
 // @namespace    http://tampermonkey.net/
-// @version      0.0.16.GIT_COUNT+GIT_HASH
+// @version      0.0.17.GIT_COUNT+GIT_HASH
 // @description  Overlays tiles on wplace.live. Can also resize, and color-match your overlay to wplace's palette. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with Wplace.live in any way, use at your own risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under GPLv3.
-// @author       ConfiG, shinkonet
+// @author       ConfiG, shinkonet, Maix
 // @match        https://wplace.live/*
 // @license      GPLv3
 // @grant        GM_setValue
@@ -14,6 +14,7 @@
 // @grant        unsafeWindow
 // @connect      *
 // @run-at       document-start
-// @downloadURL  https://github.com/cgytrus/configs-epic-overlay-pro-fork/releases/download/development/configs-epic-overlay-pro-fork.user.js
-// @updateURL    https://github.com/cgytrus/configs-epic-overlay-pro-fork/releases/download/development/configs-epic-overlay-pro-fork.user.js
+// @downloadURL  https://github.com/MaixGit/configs-epic-overlay-pro-fork-fix/releases/download/development/configs-epic-overlay-pro-fork.user.js
+// @updateURL    https://github.com/MaixGit/configs-epic-overlay-pro-fork-fix/releases/download/development/configs-epic-overlay-pro-fork.user.js
 // ==/UserScript==
+
